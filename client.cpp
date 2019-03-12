58a9d71265dd65904e260d2f0c94c608dc7f654b
Andrew Musk <andrewdmusk@gmail.com>
2019-03-12 15:44:39 -0700

    Fixed overwritten client

--- client.cpp
+++ client.cpp
@@ -1,3 +1,11 @@
+/*
+Things to work on:
+Do they need to be sequentially sending
+Output in the beginning
+DO we need the thing about received recently 
+Retransmitting packets - based off congestion window
+*/
+
 #include <iostream>
 #include <sstream>
 #include <sys/types.h>
@@ -39,6 +47,8 @@ void ehandler(char indicator, int errorCode);
 void sighandler(int num);
 bool checkTimeExpired(timeval t1, bool is10SecTimeout);
 void closeClient();
+void printPacket(Packet current, bool send, int congestion_window, int SS_thresh, bool restransmit);
+
 
 int main(int argc, char ** argv){
   //// Congestion Window 
@@ -125,7 +135,7 @@ int main(int argc, char ** argv){
     poll(pollstatus, 1, 0);
     if (pollstatus[0].revents & POLLOUT){
       numSent = sendto(sockFD, (char*) p1.create_network_packet(), 12, 0,(struct sockaddr*)&sa, sizeof sa);
-      std::cout << "numSent : " << numSent << std::endl;
+      printPacket(p1,true,congestion_window,SS_thresh,false);
       gettimeofday(&timeLastReceived, 0);
       gettimeofday(&retransmissionTimer, 0);
       break;
@@ -147,15 +157,17 @@ int main(int argc, char ** argv){
       }
       gettimeofday(&timeLastReceived, 0);
       gettimeofday(&retransmissionTimer, 0);
-      printf("Received : %d bytes\n", (int)recsize);
+      //printf("Received : %d bytes\n", (int)recsize);
       p2 = Packet((void*) buffer, recsize);
+      printPacket(p2,false,congestion_window,SS_thresh,false);
       break;
     }
     if (expired){
       sendto(sockFD, (char*) p1.create_network_packet(), 12, 0,(struct sockaddr*)&sa, sizeof sa);
-      std::cout << "time is " << retransmissionTimer.tv_sec << " " << retransmissionTimer.tv_usec << std::endl;
+      //std::cout << "time is " << retransmissionTimer.tv_sec << " " << retransmissionTimer.tv_usec << std::endl;
       gettimeofday(&retransmissionTimer, 0);
-      std::cout << "numSent : " << numSent << std::endl;
+      printPacket(p1,true,congestion_window,SS_thresh,true);
+      //std::cout << "numSent : " << numSent << std::endl;
     }
   }
 
@@ -165,7 +177,8 @@ int main(int argc, char ** argv){
   ackNum = p2.m_sequenceNum + 1;
   Packet p3 = Packet(sequenceNum, ackNum, connectionID, 1, 0, 0, NULL);
   numSent = sendto(sockFD, (char*) p3.create_network_packet(), 12, 0,(struct sockaddr*)&sa, sizeof sa);
-  std::cout << "numSent : " << numSent << std::endl;
+  printPacket(p3,true,congestion_window,SS_thresh,false);
+  //std::cout << "numSent : " << numSent << std::endl;
 
   //byte_last_sent = sequenceNum;
   //////
@@ -173,9 +186,9 @@ int main(int argc, char ** argv){
   //////
   FILE * clientFile = fopen(FILENAME, "rb");
 
-  bool receivedRecently = false;
+  //bool receivedRecently = false;
 
-  std::cout << "HANDSHAKE IS DONE" << std::endl;
+  //std::cout << "HANDSHAKE IS DONE" << std::endl;
 
 
   while (!all_packets_ack)
@@ -191,9 +204,9 @@ int main(int argc, char ** argv){
     checkTimeExpired(timeLastReceived, true);
     bool expired = checkTimeExpired(retransmissionTimer, false);
 
-    if(!packets_in_network.empty()){
-    std::cout << "The top of the packet queue has sequence num: " << packets_in_network.front().m_sequenceNum << std::endl;
-    std::cout << "The last byte that was acknowledged was: " << byte_last_acked << std::endl;}  
+    //if(!packets_in_network.empty()){
+    //std::cout << "The top of the packet queue has sequence num: " << packets_in_network.front().m_sequenceNum << std::endl;
+    //std::cout << "The last byte that was acknowledged was: " << byte_last_acked << std::endl;}  
 
     // receive packet
     poll(pollstatus, 1, 0);
@@ -203,10 +216,9 @@ int main(int argc, char ** argv){
         ehandler('r', -1);
       }
       gettimeofday(&timeLastReceived, 0);
-      printf("RECV : %d bytes\n", (int)recsize);
+      
       Packet * p1 = new Packet((void*) buffer, recsize);
-
-      receivedRecently = true;
+      //receivedRecently = true;
 
       ackNum = p1->m_sequenceNum;
       byte_last_acked = p1->m_ackNum;
@@ -215,7 +227,7 @@ int main(int argc, char ** argv){
       if(congestion_window < SS_thresh)
       {
         congestion_window += 512;
-        std::cout << "Slow Start: " << congestion_window << std::endl;
+        //std::cout << "Slow Start: " << congestion_window << std::endl;
       } else if (congestion_window >= SS_thresh)
       {
         congestion_window += ((512*512)/congestion_window);
@@ -223,8 +235,10 @@ int main(int argc, char ** argv){
         {
           congestion_window = 51200;
         }
-        std::cout << "Congestion Avoidance: " << congestion_window << std::endl;
+        //std::cout << "Congestion Avoidance: " << congestion_window << std::endl;
       }
+
+      printPacket(*p1,false,congestion_window,SS_thresh,false);
 
       if(!packets_in_network.empty())
       {
@@ -263,11 +277,11 @@ int main(int argc, char ** argv){
       //this is just checking standard 512
       if (bytes_in_network + 512 > congestion_window)
       {
-        std::cout << "Skipping due to congestion control: "<< std::endl << 
-        "Bytes in network: " << bytes_in_network << std::endl <<
-        "Byte last sent: " << byte_last_sent << std::endl <<
-        "Byte last acked: " << byte_last_acked << std::endl <<
-        "Congestion window: " << congestion_window << std::endl;
+        // std::cout << "Skipping due to congestion control: "<< std::endl << 
+        // "Bytes in network: " << bytes_in_network << std::endl <<
+        // "Byte last sent: " << byte_last_sent << std::endl <<
+        // "Byte last acked: " << byte_last_acked << std::endl <<
+        // "Congestion window: " << congestion_window << std::endl;
       }
       else {
         char fileData[512] = {0};
@@ -276,38 +290,53 @@ int main(int argc, char ** argv){
         if (bytesRead < 0){
           ehandler('r', -1);
         }
+        // DO WE NEED THIS RECEIVED RECENTLY 
         Packet outgoingPacket;
-        if(receivedRecently) {
-          outgoingPacket = Packet(sequenceNum, ackNum, connectionID, 1, 0, 0, fileData);
-          sizeMap[sequenceNum] = bytesRead + 12;
-          void* netOutgoingPacket = outgoingPacket.create_network_packet();
-          sendto(sockFD, (void*)netOutgoingPacket, (bytesRead + 12), 0,(struct sockaddr*)&sa, sizeof sa);
-          sequenceNum += bytesRead;
-          if (sequenceNum > MAX_SEQUENCE_NUMBER){
-            sequenceNum = sequenceNum - MAX_SEQUENCE_NUMBER - 1;
-          } 
-          receivedRecently = false;
-        } else {
-          outgoingPacket = Packet(sequenceNum, 0, connectionID, 0, 0, 0, fileData);
-          sizeMap[sequenceNum] = bytesRead + 12;
-          void* netOutgoingPacket = outgoingPacket.create_network_packet();
-          sendto(sockFD, (void*)netOutgoingPacket, (bytesRead + 12), 0,(struct sockaddr*)&sa, sizeof sa);
-          sequenceNum += bytesRead;
-          if (sequenceNum > MAX_SEQUENCE_NUMBER){
-            sequenceNum = sequenceNum - MAX_SEQUENCE_NUMBER - 1;
-          } 
-        }
+        // if(receivedRecently) {
+        //   outgoingPacket = Packet(sequenceNum, 0, connectionID, 0, 0, 0, fileData);
+        //   sizeMap[sequenceNum] = bytesRead + 12;
+        //   void* netOutgoingPacket = outgoingPacket.create_network_packet();
+        //   sendto(sockFD, (void*)netOutgoingPacket, (bytesRead + 12), 0,(struct sockaddr*)&sa, sizeof sa);
+        //   sequenceNum += bytesRead;
+        //   if (sequenceNum > MAX_SEQUENCE_NUMBER){
+        //     sequenceNum = sequenceNum - MAX_SEQUENCE_NUMBER - 1;
+        //   } 
+        //   receivedRecently = false;
+        // } else {
+        //   outgoingPacket = Packet(sequenceNum, 0, connectionID, 0, 0, 0, fileData);
+        //   sizeMap[sequenceNum] = bytesRead + 12;
+        //   void* netOutgoingPacket = outgoingPacket.create_network_packet();
+        //   sendto(sockFD, (void*)netOutgoingPacket, (bytesRead + 12), 0,(struct sockaddr*)&sa, sizeof sa);
+        //   sequenceNum += bytesRead;
+        //   if (sequenceNum > MAX_SEQUENCE_NUMBER){
+        //     sequenceNum = sequenceNum - MAX_SEQUENCE_NUMBER - 1;
+        //   } 
+        // }
+
+        outgoingPacket = Packet(sequenceNum, 0, connectionID, 0, 0, 0, fileData);
+        sizeMap[sequenceNum] = bytesRead + 12;
+        void* netOutgoingPacket = outgoingPacket.create_network_packet();
+        sendto(sockFD, (void*)netOutgoingPacket, (bytesRead + 12), 0,(struct sockaddr*)&sa, sizeof sa);
+        sequenceNum += bytesRead;
+        if (sequenceNum > MAX_SEQUENCE_NUMBER){
+          sequenceNum = sequenceNum - MAX_SEQUENCE_NUMBER - 1;
+        } 
 
         byte_last_sent = sequenceNum;
-        packets_in_network.push(outgoingPacket);
-        std::cout << "Sending " << bytesRead << " with sequence num : " << sequenceNum - bytesRead << " with size " << bytesRead << std::endl;
+        // CHECKING THE SIZE OF THE PACKET BEFORE PUSHING
+        if(bytesRead > 0)
+        {
+          packets_in_network.push(outgoingPacket);
+        }
+        printPacket(outgoingPacket,true,congestion_window,SS_thresh,false);
       }
     } else if (expired || retransmiting)
     {
       if(expired)
       {
-        std::cout << "We have timed out and the current size of the restransmit queue is " << packets_in_network.size() << std::endl
-        << std::endl<< std::endl<< std::endl<< std::endl<< std::endl;
+        std::cout << "We have expired and we will retransmit " << packets_in_network.size() << " packets " << std::endl << std::endl;
+        //std::cout << "We have timed out and the current size of the restransmit queue is " << packets_in_network.size() << std::endl
+        //<< std::endl<< std::endl<< std::endl<< std::endl<< std::endl;
         SS_thresh = congestion_window / 2;
         congestion_window = 512;
         retransmit_queue = packets_in_network;
@@ -322,6 +351,7 @@ int main(int argc, char ** argv){
           void* netOutgoingPacket = current.create_network_packet();
           int size = sizeMap[current.m_sequenceNum];
           sendto(sockFD, (void*)netOutgoingPacket, size , 0,(struct sockaddr*)&sa, sizeof sa);
+          printPacket(current,true,congestion_window,SS_thresh,true);
           //check if this is fine
           byte_last_retransmitted = current.m_sequenceNum + size;
           if (byte_last_retransmitted > MAX_SEQUENCE_NUMBER){
@@ -336,8 +366,8 @@ int main(int argc, char ** argv){
         bytes_in_network = (MAX_SEQUENCE_NUMBER - byte_last_acked) + byte_last_sent;
       }
 
-      std::cout << "We are in the retransmission phase and the current size of the restransmit queue is " << retransmit_queue.size() << std::endl
-      << std::endl<< std::endl<< std::endl<< std::endl<< std::endl;
+      //std::cout << "We are in the retransmission phase and the current size of the restransmit queue is " << retransmit_queue.size() << std::endl
+      //<< std::endl<< std::endl<< std::endl<< std::endl<< std::endl;
 
       while(!retransmit_queue.empty() && (bytes_in_network + (sizeMap[retransmit_queue.front().m_sequenceNum]) < congestion_window))
       {
@@ -347,10 +377,19 @@ int main(int argc, char ** argv){
         void* netOutgoingPacket = current.create_network_packet();
         int size = sizeMap[current.m_sequenceNum];
         sendto(sockFD, (void*)netOutgoingPacket, size, 0,(struct sockaddr*)&sa, sizeof sa);
+        printPacket(current,true,congestion_window,SS_thresh,true);
+
         byte_last_retransmitted = current.m_sequenceNum + size;
         if (byte_last_retransmitted > MAX_SEQUENCE_NUMBER){
             byte_last_retransmitted = byte_last_retransmitted - MAX_SEQUENCE_NUMBER - 1;
         } 
+
+        bytes_in_network = byte_last_retransmitted - byte_last_acked;
+        if(bytes_in_network < 0)
+        {
+          bytes_in_network = (MAX_SEQUENCE_NUMBER - byte_last_acked) + byte_last_sent;
+        }
+
       }
 
       if(retransmit_queue.empty())
@@ -359,7 +398,7 @@ int main(int argc, char ** argv){
       }
 
     }
-    std::cout << "------------" << std::endl;
+    //std::cout << "------------" << std::endl;
   }
 
 
@@ -370,8 +409,7 @@ int main(int argc, char ** argv){
   // send FIN 
   Packet p5 = Packet(sequenceNum, 0, connectionID, 0, 0, 1, NULL);
   numSent = sendto(sockFD, (char*) p5.create_network_packet(), 12, 0,(struct sockaddr*)&sa, sizeof sa);
-  std::cout << "numSent : " << numSent << "This is the FIN" << std::endl;
-
+  printPacket(p5, true, congestion_window, SS_thresh,false);
   // receive server's FIN ACK
   while(1){
     char buffer[524];
@@ -384,11 +422,8 @@ int main(int argc, char ** argv){
       ehandler('r', recsize);
 
       Packet p6 = Packet((void*) buffer, recsize);
-      std::cout << "RECV " << p6.m_sequenceNum << " " << p6.m_ackNum << " " << p6.m_connectionID;
-      if (p6.m_ackFlag == 1){ std::cout << " ACK"; }
-      if (p6.m_synFlag == 1){ std::cout << " SYN"; }
-      if (p6.m_finFlag == 1){ std::cout << " FIN"; }
-      std::cout << std::endl;
+      printPacket(p6, false, congestion_window, SS_thresh,false);
+
 
       // sequenceNum = p6.m_ackNum;
       ackNum = p6.m_sequenceNum + 1;
@@ -400,14 +435,14 @@ int main(int argc, char ** argv){
     if (expired){
       sendto(sockFD, (char*) p5.create_network_packet(), 12, 0,(struct sockaddr*)&sa, sizeof sa);
       gettimeofday(&retransmissionTimer, 0);
-      std::cout << "numSent : " << numSent << std::endl;
+      printPacket(p5, true, congestion_window, SS_thresh,false);
     }
   }
 
   // send ACK for server's FIN ACK
   Packet p7 = Packet(sequenceNum + 1, ackNum, connectionID, 1, 0, 0, NULL);
   numSent = sendto(sockFD, (char*) p7.create_network_packet(), 12, 0,(struct sockaddr*)&sa, sizeof sa);
-  std::cout << "numSent : " << numSent << std::endl;
+  printPacket(p7, true, congestion_window, SS_thresh,false);
 
   // 2 second wait period
   while (1){
@@ -422,14 +457,11 @@ int main(int argc, char ** argv){
 
       // resend ACK for server's FIN ACK
       if(p9.m_finFlag) {
-        std::cout << "RECV " << p9.m_sequenceNum << " " << p9.m_ackNum << " " << p9.m_connectionID;
-        if (p9.m_ackFlag == 1){ std::cout << " ACK"; }
-        if (p9.m_synFlag == 1){ std::cout << " SYN"; }
-        if (p9.m_finFlag == 1){ std::cout << " FIN"; }
-        std::cout << std::endl;
+        printPacket(p9, false, congestion_window, SS_thresh,false);
+
 
         numSent = sendto(sockFD, (char*) p7.create_network_packet(), 12, 0,(struct sockaddr*)&sa, sizeof sa);
-        std::cout << "numSent : " << numSent << std::endl;
+        printPacket(p7, true, congestion_window, SS_thresh,false);
       } else { // drop all non-FIN packets
         std::cout << "DROP " << p9.m_sequenceNum << " " << p9.m_ackNum << " " << p9.m_connectionID;
         if (p9.m_ackFlag == 1){ std::cout << " ACK"; }
@@ -482,7 +514,7 @@ bool checkTimeExpired(timeval t1, bool is10SecTimeout){
 }
 
 void closeClient(){
-  std::cout << "Closing this bitch" << std::endl;
+  //std::cout << "Closing" << std::endl;
   exit(1);
 }
 
@@ -529,4 +561,26 @@ void ehandler(char indicator, int errorCode){
 void sighandler(int num){
    std::cerr << "Signal number : " << num << " ended the client process." << std::endl;
    exit(0);  
+}
+
+void printPacket(Packet current, bool send, int congestion_window, int SS_thresh, bool retransmit)
+{
+  if(send)
+  {
+    std::cout << "SEND " << current.m_sequenceNum << " " <<  current.m_ackNum << " " << current.m_connectionID << " " 
+    << congestion_window << " " << SS_thresh;
+    if (current.m_ackFlag == 1){ std::cout << " ACK"; }
+    if (current.m_synFlag == 1){ std::cout << " SYN"; }
+    if (current.m_finFlag == 1){ std::cout << " FIN"; }
+    if (retransmit){ std::cout << " RETRANSMIT"; }
+    std::cout << std::endl;
+  } else
+  {
+    std::cout << "RECV " << current.m_sequenceNum << " " <<  current.m_ackNum << " " <<current.m_connectionID << " " 
+    << congestion_window << " " << SS_thresh;
+    if (current.m_ackFlag == 1){ std::cout << " ACK"; }
+    if (current.m_synFlag == 1){ std::cout << " SYN"; }
+    if (current.m_finFlag == 1){ std::cout << " FIN"; }
+    std::cout << std::endl;
+  }
 }
